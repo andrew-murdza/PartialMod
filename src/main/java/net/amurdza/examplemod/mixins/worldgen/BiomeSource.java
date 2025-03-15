1ee78e9f0c80bdd49d73aea822547870f05bb868
andre <andrewmurdza@gmail.com>
2025-03-15 17:29:45 -0400

    First Commit

--- src/main/java/net/amurdza/examplemod/mixins/worldgen/BiomeSource.java
+++ src/main/java/net/amurdza/examplemod/mixins/worldgen/BiomeSource.java
@@ -1,16 +1,12 @@
 package net.amurdza.examplemod.mixins.worldgen;
 
-import com.mojang.datafixers.util.Either;
 import com.mojang.datafixers.util.Pair;
-import net.amurdza.examplemod.AOEMod;
 import net.amurdza.examplemod.mixins.access.WorldGenValuesAccessor;
 import net.minecraft.core.Holder;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Climate;
 import net.minecraft.world.level.biome.MultiNoiseBiomeSource;
-import net.minecraft.world.level.biome.MultiNoiseBiomeSourceParameterList;
 import net.minecraft.world.level.levelgen.DensityFunction;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -19,46 +15,28 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
-import java.util.Random;
 
 @Mixin(value = MultiNoiseBiomeSource.class,priority = 1001)
 public abstract class BiomeSource {
-    @Shadow @Final private Either<Climate.ParameterList<Holder<Biome>>, Holder<MultiNoiseBiomeSourceParameterList>> parameters;
-
     @Shadow protected abstract Climate.ParameterList<Holder<Biome>> parameters();
 
     @Unique
-    public Holder<Biome> september2024PartialMod$getNoiseBiome(Climate.TargetPoint pTargetPoint) {
-        return parameters().findValue(pTargetPoint);
-    }
-    @Unique
     private Holder<Biome> september2024PartialMod$getNoiseBiomeHelper(int pX, int pY, int pZ, Climate.Sampler pSampler){
-//        boolean flag=new Random().nextInt(200000)==0;
-//        if(flag){
-//            AOEMod.LOGGER.info("this ran 1");
-//        }
-        if(parameters.right().isPresent()){
-            List<Pair<Climate.ParameterPoint, Holder<Biome>>> pairs = ((WorldGenValuesAccessor)parameters.right().get().get().parameters()).getValues();
-            double continentalness=pSampler.continentalness().compute(new DensityFunction.SinglePointContext(pX, pY, pZ))*10000;
-            for(Pair<Climate.ParameterPoint, Holder<Biome>> pair:pairs){
-                Climate.ParameterPoint point=pair.getFirst();
-                float maxY=point.temperature().max();
-                float minY=point.temperature().min();
-                float minCont=point.continentalness().min();
-                float maxCont=point.continentalness().max();
-                float pYfloat= (float) (pY + 64) /384*10000;
-//                if(flag){
-//                    AOEMod.LOGGER.info("{} {} {} {} {} {}", pYfloat, maxY, minY, continentalness, minCont, maxCont);
-//                }
-                if(pYfloat >= minY&&pYfloat <= maxY&&continentalness > minCont&&continentalness > maxCont){
-//                    if(flag){
-//                        AOEMod.LOGGER.info("this ran 3");
-//                    }
-                    return pair.getSecond();
-                }
+        List<Pair<Climate.ParameterPoint, Holder<Biome>>> pairs = ((WorldGenValuesAccessor)parameters()).getValues();
+        double continentalness=pSampler.continentalness().compute(new DensityFunction.SinglePointContext(pX, pY, pZ));
+        List<Float> floats=List.of(-0.38F,-0.1F,0.1F,0.38F);
+        List<Integer> ys=List.of(-34,-5,24,53);
+        for(int i=0;i<floats.size();i++){
+            if(continentalness<floats.get(i)){
+                return pairs.get(i).getSecond();
             }
         }
-        return september2024PartialMod$getNoiseBiome(pSampler.sample(pX,pY,pZ));
+        for(int i=floats.size();i<floats.size()+ys.size();i++){
+            if(pY<=ys.get(i-floats.size())){
+                return pairs.get(i).getSecond();
+            }
+        }
+        return pairs.get(pairs.size()-1).getSecond();
     }
     @Inject(
             method = {"getNoiseBiome(IIILnet/minecraft/world/level/biome/Climate$Sampler;)Lnet/minecraft/core/Holder;"},
